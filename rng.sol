1da74ed6a338ab22030a5cfa34e542139fcc502c
Greg Colvin <greg@colvin.org>
2019-04-12 18:49:40 -0600

    Update rng.sol

--- rng.sol
+++ rng.sol
@@ -12,14 +12,14 @@ contract rng {
                     	    		0xdba29b85a759fb10848f01256f2b5c39,
 		                   			0xa9390182a3a79447c8394ae189edcfc1,
 									0xd66ec8a137fb01fae6b7a83c83d0adf9];
-
+		uint256 mix = gasleft();
 		for (uint i = 0; i < 32768; ++i) {
 			uint8[32][5] memory out;
 
 			// need 32 bytes of output for each log argument
 			for (uint256 j = 0; j < 32; ++j) {
 			
-				// harvest low order byte of 5 Blum Blum Shubs for 5-word log output
+				// harvest bytes from 5 Blum Blum Shubs for 5-word log output
 				for (uint k = 0; k < 5; ++k) {
 				
 					uint256 bbs = blum[k];
@@ -27,11 +27,10 @@ contract rng {
 					bbs %= shub;
 					blum[k] = bbs;
 					
-					out[k][j] = uint8(bbs & 255);
+					out[k][j] ^= uint8((mix ^= bbs) & 255);
 				}
-			}
-// parity chokes	
-			// log4(bytes32(out[0]), bytes32(out[1]), bytes32(out[2]), bytes32(out[3]), bytes32(out[4]));
+			}	
 		}
+		log4(bytes32(out[0]), bytes32(out[1]), bytes32(out[2]), bytes32(out[3]), bytes32(out[4]));
 	}
 }
